22d75b19c8b242d67c6fdb834b058cd0be543708
Stefan Majer <stefan.majer@f-i-ts.de>
2021-07-29 16:09:35 +0200

    save before vacation

--- cmd/go.mod
+++ cmd/go.mod
@@ -4,5 +4,7 @@ go 1.16
 
 require (
 	github.com/Masterminds/semver/v3 v3.1.1
+	github.com/google/go-github/v37 v37.0.0
 	github.com/mmcdole/gofeed v1.1.3
+	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
 )
--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 	"os"
 	"strconv"
 	"strings"
 
 	"github.com/Masterminds/semver/v3"
+	"github.com/google/go-github/v37/github"
 	"github.com/mmcdole/gofeed"
+	"golang.org/x/oauth2"
 )
 
 func main() {
@@ -23,6 +27,20 @@ func main() {
 	if supportlevel == "" {
 		supportlevel = "longterm"
 	}
+
+	kv, ok := getNextKernelVersion(major, minor, supportlevel)
+	if !ok {
+		fmt.Println("no new kernel found")
+	}
+	fmt.Printf("new kernel:%s\n", kv)
+
+	err := createTag("5.10.48-65")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getNextKernelVersion(major, minor int, supportLevel string) (string, bool) {
 	fp := gofeed.NewParser()
 	feed, _ := fp.ParseURL("https://www.kernel.org/feeds/kdist.xml")
 	for _, item := range feed.Items {
@@ -37,11 +55,44 @@ func main() {
 			continue
 		}
 		isOdd := version.Minor()%2 == 0
-		if version.Major() == uint64(major) && version.Minor() == uint64(minor) && support == supportlevel && isOdd {
+		if version.Major() == uint64(major) && version.Minor() == uint64(minor) && support == supportLevel && isOdd {
 			// This is the next desired kernel version we would like to release
 			// TODO: create a tag with this version and modify the action to build the kernel based on the tag name
-			fmt.Println(version)
-			return
+			return version.String(), true
 		}
 	}
+	return "", false
+}
+
+func createTag(name string) error {
+	ctx := context.Background()
+	token := os.Getenv("GITHUB_TOKEN")
+	client := github.NewClient(nil)
+	if token != "" {
+		ts := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: token},
+		)
+		tc := oauth2.NewClient(ctx, ts)
+
+		client = github.NewClient(tc)
+	}
+
+	tag, resp, err := client.Git.GetTag(ctx, "metal-stack", "kernel", name)
+	if err != nil && resp.StatusCode != http.StatusNotFound {
+		return err
+	}
+	if resp.StatusCode == http.StatusNotFound {
+		fmt.Println("not found")
+	} else {
+	}
+	fmt.Printf("tag exists: %v\n", tag)
+
+	// tag := &github.Tag{
+	// 	Tag: &name,
+	// }
+	// _, _, err := client.Git.CreateTag(ctx, "metal-stack", "kernel", tag)
+	// if err != nil {
+	// 	return err
+	// }
+	return nil
 }
