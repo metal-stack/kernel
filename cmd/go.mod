00105ec024fd8c1a82b9f2a2e827ee452fbd774f
Stefan Majer <stefan.majer@f-i-ts.de>
2022-01-23 13:26:15 +0100

    simplification

--- cmd/go.mod
+++ cmd/go.mod
@@ -1,10 +1,18 @@
 module github.com/metal-stack/kernel
 
-go 1.16
+go 1.17
 
 require (
 	github.com/Masterminds/semver/v3 v3.1.1
 	github.com/google/go-github/v37 v37.0.0
-	github.com/mmcdole/gofeed v1.1.3
-	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
+)
+
+require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/net v0.0.0-20220121210141-e204ce36a2ba // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
 )
--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,42 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"strconv"
-	"strings"
+	"time"
 
 	"github.com/Masterminds/semver/v3"
 	"github.com/google/go-github/v37/github"
-	"github.com/mmcdole/gofeed"
 	"golang.org/x/oauth2"
 )
+
+type KernelVersions struct {
+	LatestStable struct {
+		Version string `json:"version"`
+	} `json:"latest_stable"`
+	Releases []struct {
+		Iseol    bool        `json:"iseol"`
+		Version  string      `json:"version"`
+		Moniker  string      `json:"moniker"`
+		Source   string      `json:"source"`
+		Pgp      interface{} `json:"pgp"`
+		Released struct {
+			Timestamp int    `json:"timestamp"`
+			Isodate   string `json:"isodate"`
+		} `json:"released"`
+		Gitweb    string      `json:"gitweb"`
+		Changelog interface{} `json:"changelog"`
+		Diffview  string      `json:"diffview"`
+		Patch     struct {
+			Full        string      `json:"full"`
+			Incremental interface{} `json:"incremental"`
+		} `json:"patch"`
+	} `json:"releases"`
+}
 
 func main() {
 	major, _ := strconv.Atoi(os.Getenv("KERNEL_MAJOR"))
@@ -31,6 +56,7 @@ func main() {
 	kv, ok := getNextKernelVersion(major, minor, supportlevel)
 	if !ok {
 		fmt.Println("no new kernel found")
+		return
 	}
 	fmt.Printf("new kernel:%s\n", kv)
 
@@ -40,22 +66,54 @@ func main() {
 	}
 }
 
+func getKernelVersions() (*KernelVersions, error) {
+	url := "https://www.kernel.org/releases.json"
+	c := http.Client{
+		Timeout: time.Second * 2, // Timeout after 2 seconds
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	res, err := c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	kv := KernelVersions{}
+	err = json.Unmarshal(body, &kv)
+	if err != nil {
+		return nil, err
+	}
+	return &kv, nil
+}
+
 func getNextKernelVersion(major, minor int, supportLevel string) (string, bool) {
-	fp := gofeed.NewParser()
-	feed, _ := fp.ParseURL("https://www.kernel.org/feeds/kdist.xml")
-	for _, item := range feed.Items {
-		parts := strings.Split(item.Title, ": ")
-		if len(parts) != 2 {
+	kv, err := getKernelVersions()
+	if err != nil {
+		return "", false
+	}
+	for _, r := range kv.Releases {
+		if r.Moniker != supportLevel {
 			continue
 		}
-		kernelVersion := parts[0]
-		support := parts[1]
+		kernelVersion := r.Version
 		version, err := semver.NewVersion(kernelVersion)
 		if err != nil {
 			continue
 		}
-		isOdd := version.Minor()%2 == 0
-		if version.Major() == uint64(major) && version.Minor() == uint64(minor) && support == supportLevel && isOdd {
+		isOdd := version.Patch()%2 == 0
+		if !isOdd {
+			continue
+		}
+		if version.Major() == uint64(major) && version.Minor() == uint64(minor) {
 			// This is the next desired kernel version we would like to release
 			// TODO: create a tag with this version and modify the action to build the kernel based on the tag name
 			return version.String(), true
